e63f8e2998603125a2ac4f6a0939ad2821fa9f1f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-12 17:57:37 +0100

    Added code to confront the word with its reverse to understand if they are the same.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 //Chiedere all'utente di inserire una parola.
 const userWord = prompt('Inserisci una parola e scopri se è palindroma'); //String
-//Scrittura variabile che controllerà se la parola sia palindroma o meno.
-const palindrome = false;
 
 //Scrittura funzione.
 function isPalindrome(string){
     //Rendere il testo tutto minuscolo.
     const lowerCaseUserWord = userWord.toLowerCase();
     //Dichiarazione delle variabili che serviranno a confrontare le parole.
-    let word = letterIndex,
-    ReversedWord = letterReverseIndex
+    let letterIndex = ''
+    let letterReverseIndex = ''
     
-
     for(let i = 0; i < lowerCaseUserWord.length; i++){
-        const letterIndex = lowerCaseUserWord[i]; //String
-           
+        letterIndex = lowerCaseUserWord[i]; //String
+   
     }
 
     for(let reverseI = lowerCaseUserWord.length - 1; reverseI >= 0; reverseI--){
-        const letterReverseIndex = lowerCaseUserWord[reverseI];//String
+        letterReverseIndex = lowerCaseUserWord[reverseI];//String
 
     }
 
+    //Confronto delle due parole.
     
-    
+    if(letterIndex === letterReverseIndex ){
+        console.log(userWord + ' è una parola palindroma');
+        
+    } else {
+        console.log(userWord + ' non è una parola palindroma');
+    }
 }
 
 isPalindrome(userWord);
