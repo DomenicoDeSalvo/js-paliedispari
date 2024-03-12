b1e982091e53c8ae78e5c2459aa1d4056b2448e4
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-13 00:40:53 +0100

    the game is able to display the result of the sum

--- Pari e Dispari/js/app.js
+++ Pari e Dispari/js/app.js
@@ -28,30 +28,57 @@ userBet = 'even'; //String
 //Acquisire il numero scelto dall'utente.
 const inputNumberChoiceElement = document.querySelector('.user_number');//Element | Null
 //Dichiarare la variabile a cui assegnarlo.
-let userNumber = ''; //Number
+let userNumber = ''; //String
+//Dichiarare una variabile per il numero che sarà assegnato al computer.
+let computerNumber = ''//String
 //Acquisizione del bottone per confermare la scelta del numero.
 const submitElement = document.querySelector('.submit'); //Element || Null.
 //Cliccando il bottone, il numero scelto dall'utente verrà assegnato alla variabile userNumber e si passerà alla fase finale.
 //Acquisire la classe della terza fase.
-const outcomeElement = document.querySelector('.outcome'); //Element || Null
+const resultElement = document.querySelector('.result'); //Element || Null
 
 submitElement.addEventListener('click', function(){
-    userNumber = inputNumberChoiceElement.value //string
+    userNumber = parseInt(inputNumberChoiceElement.value) //string
     secondtPartElement.classList.add('hidden'); //Element || Null
-    outcomeElement.classList.remove('hidden'); //Element || Null
-    console.log(userNumber);
+    resultElement.classList.remove('hidden'); //Element || Null
+    resultElement.classList.add('shown'); //Element || Null
+    //Atribuire un numero al computer.
+    computerNumber = parseInt(Math.floor(Math.random() * 5 + 1)); //Number
+    console.log(userNumber, computerNumber);
+    //Insermiento della funzione.
+    const result = isSumEven(userNumber, computerNumber);//String
+    console.log(result);
+    //Mostrare i risultati attribuiti ai giocatori.
+    const userElement = document.querySelector('.number_user'); //Element | Null
+    const computerElement = document.querySelector('.number_computer'); //Element | Null
+    userElement.innerHTML = userNumber; //number
+    computerElement.innerHTML = computerNumber; //Number
+    //Ed il totale.
+    const sumElement = document.querySelector('.number_sum'); //Element | Null
+    sumElement.innerHTML = sum(userNumber, computerNumber); //Number
     
 })
 
-//Creazione della funzione.
-function isSumEven (userNumber, computerNumber){
-    const sum = userNumber + computerNumber;
+
+// FUNZIONI
+//Creazione della funzione necessaria a capire se il numero sia pari o meno.
+function isSumEven (number1, number2){
+    const sum = number1 + number2;
     const rest = sum % 2;
 
     if (rest === 0) {
-        return true
+        return true 
     } else {
-        return false
+        return false 
     }
 }
+
+//Funzione per la somma.
+function sum (number1, number2){
+    const sum = number1 + number2;
+
+    return sum
+}
+
+
 
